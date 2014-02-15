f544dd17605c3b47e083c0f671856f223f9f3257
Dan M <the.code.writer@gmail.com>
2014-02-14 17:45:00 -0700

    Added support for trial version of Swype.  Added better logging and exception handling.  Added APK archive

--- Source/src/com/danation/xposed/swypetweaks/SwypeTweaks.java
+++ Source/src/com/danation/xposed/swypetweaks/SwypeTweaks.java
@@ -5,27 +5,41 @@ import java.lang.reflect.Method;
 import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class SwypeTweaks implements IXposedHookLoadPackage {
+public class SwypeTweaks implements IXposedHookLoadPackage
+{
 	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		
+	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
+	{	
 		final ClassLoader loader = lpparam.classLoader;
+		final String logPrefix = "SwypeTweaks: ";
 		
-		if (lpparam.packageName.equals("com.nuance.swype.dtc")) {
+		if (lpparam.packageName.equals("com.nuance.swype.dtc") ||
+			lpparam.packageName.equals("com.nuance.swype.trial"))
+		{	
+			XposedBridge.log(logPrefix + "Found Swype package");
 			
-			XposedHelpers.findAndHookMethod("com.nuance.swype.input.InputView", lpparam.classLoader,
-					"startSpeech", new XC_MethodReplacement() {
-				@Override
-				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+			try
+			{
+				XposedHelpers.findAndHookMethod("com.nuance.swype.input.InputView", lpparam.classLoader, "startSpeech", new XC_MethodReplacement()
 				{
-					Object mIme = XposedHelpers.getObjectField(param.thisObject, "mIme");
-					Method mImeStartVoiceRecognition = XposedHelpers.findMethodExact("com.nuance.swype.input.IME", loader, "startVoiceRecognition", String.class);
-					
-					mImeStartVoiceRecognition.invoke(mIme, "");
-					
-					return null;
-				}
-			});
+					@Override
+					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+					{
+						XposedBridge.log(logPrefix + "Replacing startSpeech() and calling startVoiceRecognition()");
+						Object mIme = XposedHelpers.getObjectField(param.thisObject, "mIme");
+						Method mImeStartVoiceRecognition = XposedHelpers.findMethodExact("com.nuance.swype.input.IME", loader, "startVoiceRecognition", String.class);
+						
+						mImeStartVoiceRecognition.invoke(mIme, "");
+						
+						return null;
+					}
+				});
+			}
+			catch (Exception ex)
+			{
+				XposedBridge.log(logPrefix + "SwypeTweaks: Caught Exception");
+				XposedBridge.log(ex);
+			}
 		}
 	}
 }
